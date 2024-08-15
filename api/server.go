b8e3ec887319305cd609d9d148a40754b8dbef0a
Theodore Myker <tmyker@THEODORE-MYKER>
2024-08-15 23:07:22 +0700

    env api

--- api/server.go
+++ api/server.go
@@ -17,14 +17,16 @@ func NewServer(store *db.Store) *Server {
 	router := gin.Default()
 
 	router.POST("/accounts", server.createAccount)
+	router.GET("/accounts/:id", server.getAccountById)
+	router.GET("/accounts", server.listAccount)
 
 	server.router = router
 	return server
 }
 
 // Start runs the HTTP server on a specific address
-func (server *Server) Start(address String) error {
-
+func (server *Server) Start(address string) error {
+	return server.router.Run(address)
 }
 
 func errorResponse(err error) gin.H {
